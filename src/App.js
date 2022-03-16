85c402edfe899d1c6712129a560b247031c66ecc
mancjames <multicolouredsmurf@gmail.com>
2022-03-16 22:47:31 +0000

    added horizontal navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './sass/main.scss';
 
+import NavigationHorizontal from './components/NavigationHorizontal';
+
 function App() {
 
   return (
     <div className="App">
       <header>
-        
+          <NavigationHorizontal />
       </header>
     </div>
   );
--- src/components/NavigationHorizontal.js
+++ src/components/NavigationHorizontal.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { ReactComponent as Logo} from '../assets/logo.svg'
 
 export default function NavigationHorizontal() {
   return (
-    <div>NavigationHorizontal</div>
+    <nav className="navhorizontal">
+         <Logo />
+        <ul className="navhorizontal__list">
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Settings</li>
+            <li>Community</li>
+        </ul>
+    </nav>
   )
 }
